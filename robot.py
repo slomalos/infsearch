fe0d647bf8fe0a939db56f624650aa20a576a4d7
slomalos <113264825+slomalos@users.noreply.github.com>
2025-12-29 17:56:15 +0300

    Update robot.py

--- robot.py
+++ robot.py
@@ -1,45 +1,39 @@
 import time
 import sys
+import yaml
 import id_fetcher
 import downloader
 import importer 
 
-BAN_SLEEP_TIME = 60.0 #Время если дали 429 (в секундах)
 
-CRAWL_DELAY = 1.5 # Параметр задержки (в секундах)
-
-TARGET_DOCS_COUNT = 70000 # То, сколько айди документов надо скачать.
-
+def load_config(path="config.yaml"):
+    try:
+        with open(path, "r") as f:
+            return yaml.safe_load(f)
+    except:
+        sys.exit(1)
 
 def run_robot():
-    print("Запуск робота")
-    print(f"Конфигурация: Задержка={CRAWL_DELAY}s, Количество документов={TARGET_DOCS_COUNT} штук")
-    print("="*30)
+    cfg = load_config()
+    
+    target = cfg['logic'].get('target_docs', 70000) # количество доков
+    delay  = cfg['logic'].get('delay', 1.0)        # время между скач
+    err    = cfg['logic'].get('error_delay', 30.0) # время если 429
+    
+    print(f"Робот запущен")
 
-    print("\n[ЭТАП 1] Сбор идентификаторов")
-    id_fetcher.MAX_DOCS = TARGET_DOCS_COUNT
+    id_fetcher.MAX_DOCS = target
+    
+    downloader.REQUEST_DELAY = delay
+    downloader.ERROR_TIMEOUT = err
+    
     id_fetcher.fetch_ids()
-    print("-> этап 1 завершен")
-
-    print("\n[ЭТАП 2] скачивание")
-    
-    downloader.REQUEST_DELAY = CRAWL_DELAY
-    downloader.ERROR_TIMEOUT = BAN_SLEEP_TIME
-    
     downloader.main()
-
-    print("\n[ЭТАП 3] импорт в MongoDB")
-    try:
-        if hasattr(importer, 'run_fast_importer'):
-            importer.run_fast_importer()
-        else:
-            importer.run_importer() 
-    except Exception as e:
-        print(f"Ошибка при импорте: {e}")
-        
-    print("-> Этап 3 завершен.")
     
-    print("\nРабота окончена")
+    if hasattr(importer, 'run_fast_importer'):
+        importer.run_fast_importer()
+    else:
+        importer.run_importer()
 
 if __name__ == "__main__":
     run_robot()
